330adca6fb7fd2d201e1e988d34a382d2b33bd72
Jonathan Motta <jonathangmotta98@gmail.com>
2024-05-04 23:01:25 -0300

    Fix: Better report

--- src/generate_report.py
+++ src/generate_report.py
@@ -16,7 +16,10 @@ def create_graph(components_data_filter, input_list_dict, spectral_list, compone
 
     component_dict = {comp: components_data_filter[comp].loc[:, ('x', 'y')] for comp in (components)}
 
+    table_dict: dict[str, list[tuple[str, str, str, str, str]]] = {}
+    
     for k, v in component_dict.items():
+        table_list: list[tuple[str, str, str, str, str]] = []
         fig, ax = plt.subplots()
         
         input_list = [input_list_dict[k]['x'].tolist(), input_list_dict[k]['y'].tolist()]
@@ -36,6 +39,9 @@ def create_graph(components_data_filter, input_list_dict, spectral_list, compone
         ax.scatter(input_list[0], input_list[1], label='Analysed File Points', zorder=2, color='orange')
 
         for i in range(number_range, 0, -1):
+            table_list.append((str(i), f'{database_list[0][number_range - i]:.2f}', f'{database_list[1][number_range - i]:.2f}', 
+                             f'{input_list[0][number_range - i]:.2f}', f'{input_list[1][number_range - i]:.2f}'))
+            
             text = ax.annotate(i, (database_list[0][number_range - i], database_list[1][number_range - i]), zorder=4, color='blue', fontsize=10, ha='left', va='center')
             text.set_path_effects([withStroke(linewidth=3, foreground='white')])
 
@@ -47,27 +53,51 @@ def create_graph(components_data_filter, input_list_dict, spectral_list, compone
 
         ax.plot(dict_teste[compound]['x'], dict_teste[compound]['y'], color='black', zorder=1)
         ax.plot(dict_teste[k]['x'], dict_teste[k]['y'], color='red', zorder=1, alpha =0.5)
-        plt.title(k)
         ax.legend()
         plt.xlim((4000, 400))
         plt.ylim((0, 100))
 
         create_temp_png(os.path.join(TEMP_PATH, f"{k}.png"), fig)
+        table_list = sorted(table_list, key=lambda x: float(x[0]))
+        table_dict[k] = table_list
 
-    create_pdf_page(TEMP_PATH, output, components)
+    create_pdf_page(TEMP_PATH, output, components, table_dict)
 
 
-def create_temp_png(output_path: str, fig: Figure):
+def create_temp_png(output_path: str, fig: Figure) -> None:
     tmp_file = os.path.join(output_path)
     fig.savefig(tmp_file)
     plt.close(fig)
 
 
-def get_temp_imgs(imgs_path: str):
+def get_temp_imgs(imgs_path: str) -> list[str]:
     return [os.path.join(imgs_path, img_name) for img_name in os.listdir(imgs_path) if img_name.endswith('.png')]
 
 
-def create_pdf_page(imgs_path: str, output_pdf: str, compound_list: list[str]):
+def footer(pdf, tmp_file):
+        pdf.set_y(-15)
+        pdf.set_font('Arial', 'I', 8)
+        pdf.cell(0, 10, os.path.basename(tmp_file).split('.')[0], 0, 0, 'C')
+
+
+def create_table(pdf: FPDF, table_data: list[tuple[str, str, str, str, str]], tmp_file: str) -> None:
+    table_header = [('Id', 'Wavelength (cm^1)', 'Intensity', 'Wavelength (cm^1)', 'Intensity')]
+    table_data = table_header + table_data
+
+    pdf.add_page()
+    pdf.set_font("Times", size=12)
+    with pdf.table(text_align="CENTER", borders_layout="SINGLE_TOP_LINE") as table:
+        for data_row in table_data:
+            row = table.row()
+            for datum in data_row:
+                row.cell(datum)
+
+    pdf.footer = lambda: footer(pdf, tmp_file)
+
+
+def create_pdf_page(imgs_path: str, output_pdf: str, 
+                    compound_list: list[str], table_dict: dict[str, list[tuple[str, str, str, str, str]]]) -> None:
+
     temp_img_list = get_temp_imgs(imgs_path)
 
     file_dict = {os.path.basename(filename).split('.')[0]: filename for filename in temp_img_list}
@@ -78,7 +108,22 @@ def create_pdf_page(imgs_path: str, output_pdf: str, compound_list: list[str]):
     
     for tmp_file in temp_img_list:
         pdf.add_page()
+        pdf.footer = lambda: footer(pdf, tmp_file)
         pdf.image(tmp_file)
+        create_table(pdf, table_dict[os.path.basename(tmp_file).split('.')[0]], tmp_file)
     pdf.output(output_pdf)
 
     [os.remove(tmp_file) for tmp_file in temp_img_list]
+
+    for i in range(1, pdf.page_no() + 1):
+        pdf.in_footer = lambda pdf: custom_footer(pdf, i)
+
+def custom_footer(pdf, page_num):
+    if page_num == 2:
+        pdf.set_y(-15)
+        pdf.set_font("Arial", size=10)
+        pdf.cell(0, 10, f"Custom Footer for Page {page_num}", align="C")
+    else:
+        pdf.set_y(-15)
+        pdf.set_font("Arial", size=10)
+        pdf.cell(0, 10, f"Footer for Page {page_num}", align="C")
