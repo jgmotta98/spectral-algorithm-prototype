2be7fe44db70ec0f621d28e2ceb3e1dbbe7938f4
Jonathan Motta <jonathangmotta98@gmail.com>
2024-05-05 02:45:49 -0300

    Maint: Information on report

--- src/generate_report.py
+++ src/generate_report.py
@@ -8,10 +8,11 @@ from fpdf import FPDF
 TEMP_PATH = './files/temp'
 
 
-def create_graph(components_data_filter, input_list_dict, spectral_list, components, compound, output):
+def create_graph(components_data_filter, input_list_dict, spectral_list, components, compound, output, band_check):
     dict_teste = {v.iloc[0, 0]: v.drop('name', axis=1) for v in spectral_list}
 
     sorted_data = dict(sorted(components.items(), key=lambda item: item[1], reverse=True))
+    percentages = list(sorted_data.values())
     components = list(sorted_data.keys())
 
     component_dict = {comp: components_data_filter[comp].loc[:, ('x', 'y')] for comp in (components)}
@@ -35,8 +36,8 @@ def create_graph(components_data_filter, input_list_dict, spectral_list, compone
 
         number_range = len(database_list[0])
 
-        ax.scatter(database_list[0], database_list[1], label='Database Points', zorder=2, color='blue')
-        ax.scatter(input_list[0], input_list[1], label='Analyzed Points', zorder=2, color='orange')
+        ax.scatter(database_list[0], database_list[1], label=f'{k.capitalize()} Points', zorder=2, color='blue')
+        ax.scatter(input_list[0], input_list[1], label=f'{compound.capitalize()} Points', zorder=2, color='orange')
 
         for i in range(number_range, 0, -1):
             table_list.append((str(i), f'{database_list[0][number_range - i]:.2f}', f'{database_list[1][number_range - i]:.2f}', 
@@ -51,11 +52,13 @@ def create_graph(components_data_filter, input_list_dict, spectral_list, compone
         for i in range(len(database_list[0])):
             ax.plot([database_list[0][i], input_list[0][i]], [database_list[1][i], input_list[1][i]], linestyle='--', color='purple', alpha=0.5, zorder=3)
 
-        ax.plot(dict_teste[compound]['x'], dict_teste[compound]['y'], label='Database Spectra', color='black', zorder=1)
-        ax.plot(dict_teste[k]['x'], dict_teste[k]['y'], color='red', label='Analyzed Spectra', zorder=1, alpha =0.5)
+        ax.plot(dict_teste[compound]['x'], dict_teste[compound]['y'], label=f'{k.capitalize()} Spectra', color='black', zorder=1)
+        ax.plot(dict_teste[k]['x'], dict_teste[k]['y'], color='red', label=f'{compound.capitalize()} Spectra', zorder=1, alpha =0.5)
         ax.legend()
         plt.xlim((4000, 400))
         plt.ylim((0, 100))
+        plt.xlabel('Wavelength (cm⁻¹)')
+        plt.ylabel('Intensity')
         figure = plt.gcf()
         figure.set_size_inches(9.5*2.2, 5.5*2.2)
 
@@ -63,7 +66,7 @@ def create_graph(components_data_filter, input_list_dict, spectral_list, compone
         table_list = sorted(table_list, key=lambda x: float(x[0]))
         table_dict[k] = table_list
 
-    create_pdf_page(TEMP_PATH, output, components, table_dict)
+    create_pdf_page(TEMP_PATH, output, components, table_dict, compound, band_check, percentages)
 
 
 def create_temp_png(output_path: str, fig: Figure) -> None:
@@ -92,7 +95,7 @@ def footer(pdf, tmp_file):
 
 
 def create_table(pdf: FPDF, table_data: list[tuple[str, str, str, str, str]], tmp_file: str) -> None:
-    table_header = [('Id', 'Database\nWavelength (cm^1)', 'Database\nIntensity', 'Analyzed\nWavelength (cm^1)', 'Analyzed\nIntensity')]
+    table_header = [('Id', 'Database\nWavelength (cm-¹)', 'Database\nIntensity', 'Analyzed\nWavelength (cm-¹)', 'Analyzed\nIntensity')]
     table_data = table_header + table_data
 
     pdf.add_page()
@@ -111,8 +114,40 @@ def create_title(pdf, title_idx, title_name):
     pdf.write(2, f'{title_idx}. {title_name.capitalize()}')
     pdf.ln(10)
 
+
+def create_similarity_table(pdf, table_data):
+    pdf.set_font("Times", size=12)
+    with pdf.table(text_align="CENTER", borders_layout="MINIMAL") as table:
+        for data_row in table_data:
+            row = table.row()
+            for datum in data_row:
+                row.cell(datum)
+
+
+def create_details_page(pdf, analyzed_comp, band_check, compound_list, percentages):
+    pdf.add_page()
+    pdf.set_font('Times', 'B', 24)
+    pdf.write(2, 'Information Details')
+    pdf.ln(40)
+
+    pdf.set_font('Times', '', 14)
+    pdf.write(2, f'Analyzed compound name: {analyzed_comp}')
+    pdf.ln(10)
+
+    pdf.write(2, f'Analyzed range: {band_check} cm-¹')
+    pdf.ln(25)
+
+    table_list = [(f'{comp_idx + 1}', comp, f'{perc:.2f}') for comp_idx, (comp, perc) in enumerate(zip(compound_list, percentages))]
+    table_list = [('Id', 'Database Compound (Most Similar to Least Similar)', 'Similarity Percentage (%)')] + table_list
+
+    create_similarity_table(pdf, table_list)
+
+    pdf.footer = lambda: footer(pdf, '')
+
+
 def create_pdf_page(imgs_path: str, output_pdf: str, 
-                    compound_list: list[str], table_dict: dict[str, list[tuple[str, str, str, str, str]]]) -> None:
+                    compound_list: list[str], table_dict: dict[str, list[tuple[str, str, str, str, str]]],
+                    analyzed_comp_name: str, band_check: int, percentages: list[str]) -> None:
 
     temp_img_list = get_temp_imgs(imgs_path)
 
@@ -124,6 +159,8 @@ def create_pdf_page(imgs_path: str, output_pdf: str,
 
     background_image_path = r'D:\Downloads\Material Faculdade\Material TCC\spectral-algorithm-prototype\files\imgs\background-1-test.png'
     pdf.set_page_background(background_image_path)
+
+    create_details_page(pdf, analyzed_comp_name, band_check, compound_list, percentages)
     
     for file_idx, tmp_file in enumerate(temp_img_list):
         pdf.add_page()
